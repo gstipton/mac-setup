cf77c8bd3212b36da383117d0433f7f316992580
Geoff Tipton <geoff@Geoffs-MBP.lan>
2018-10-11 06:34:36 -0400

    Initial

---
+++ utils.sh
@@ -0,0 +1,32 @@
+
+
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install gnu-grep --with-default-names
+brew install findutils
+
+# Install bash 4
+brew install bash
+# Add new bash to /etc/shells
+# sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
+
+LIBRARIES=(
+  ack
+  jq
+  node
+  pt
+  tmux
+  wget
+)
+
+echo "Installing packages..."
+brew install ${LIBRARIES[@]}
+
+brew cleanup
+
+CASKS=(
+  virtualbox
+)
